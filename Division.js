840ab7a81630ec3c36e970944b6c75583d1e98f8
bhavya-netizen <75616572+bhavya-netizen@users.noreply.github.com>
2021-01-12 17:41:37 +0530

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,20 @@
+class Division{
+    constructor(x, y, width, height){
+        var options ={
+          isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.color = color(random(0, 255), random(0,255), random(0, 255));
+
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(this.color);
+        rect(pos.x, pos.y, this.w, this.h);
+        
+    }
+}
---
+++ Plinko.js
@@ -0,0 +1,19 @@
+class Plinko{
+    constructor(x, y) {
+        var options = {
+            isStatic: true,
+        }
+        this.x = x;
+        this.y = y;
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        ellipseMode(RADIUS);
+        fill("skyblue");
+        noStroke();
+        ellipse(pos.x, pos.y, this.r, this.r);
+      }
+}
